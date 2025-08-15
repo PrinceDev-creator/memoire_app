c98e926e819412dfd1012778a7b3b888318477e2
Prince BOGNONKPEclear <princedev317@gmail.com>
2025-08-15 16:24:16 +0100

    ajout des fichiers statiques

--- edutrack/urls.py
+++ edutrack/urls.py
@@ -52,6 +52,7 @@ from django.urls import path, include
 from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 schema_view = get_schema_view(
    openapi.Info(
@@ -128,3 +129,5 @@ urlpatterns = [
     path('swagger/<format>/', schema_view.without_ui(cache_timeout=0), name='schema-json'),
     path('view-school-notes/', view_school_notes, name='view-school-notes'),
 ]
+
+urlpatterns += staticfiles_urlpatterns()
